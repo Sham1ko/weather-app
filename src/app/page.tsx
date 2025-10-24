dd57e1aaf9ae2760a116ba74a32d52e5147be746
Sham <shamshyrak.zholdasbek@gmail.com>
2025-10-24 13:28:48 +0200

    chore: update Tailwind CSS to version 4.1.16 and adjust PostCSS configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 
         <button
           type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mt-4"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-hidden focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mt-4"
         >
           Search
         </button>
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 export default function Card({ children, className }: CardProps) {
   return (
     <div
-      className={`p-8 flex flex-col bg-white rounded-lg border border-gray-400/20 shadow-sm backdrop-blur-md ${className}`}
+      className={`p-8 flex flex-col bg-white rounded-lg border border-gray-400/20 shadow-xs backdrop-blur-md ${className}`}
     >
       {children}
     </div>
