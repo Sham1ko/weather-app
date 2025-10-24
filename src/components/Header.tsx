64033e99c85238bbd70beb04112da32d4ab371ff
Sham <shamshyrak.zholdasbek@gmail.com>
2025-10-24 13:42:01 +0200

    refactor: simplify Header component layout and remove Link dependency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,10 @@
 "use client";
-import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="header h-16 bg-white z-10 flex items-center justify-center">
-      <div className="container flex justify-between xl:px-40">
-        <Link href="/" className="flex">
-          <h1 className="text-2xl content-center font-medium">Weather App</h1>
-        </Link>
+    <header className="flex justify-center">
+      <div className="container py-4">
+        <h1 className="text-2xl font-medium">Weather App</h1>
       </div>
     </header>
   );
